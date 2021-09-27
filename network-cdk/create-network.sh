c749677aed60fbcd1388b12bf0d30ede9be21de2
Roberto Juárez <roberto.juarez@jungheinrich.es>
2021-09-27 17:00:38 +0200

    chore: Parameterize script to make the call to cdk deploy autoapproved

--- network-cdk/create-network.sh
+++ network-cdk/create-network.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-AWS_ACCESS_KEY_ID=${1}
-AWS_SECRET_ACCESS_KEY=${2}
-AWS_DEFAULT_REGION=${3}
+REQUIRE_APPROVAL=${7:-any-change}
+
+export AWS_ACCESS_KEY_ID=${1}
+export AWS_SECRET_ACCESS_KEY=${2}
 
 cdk deploy \
-  -c accountId=${4} \
+  --require-approval $REQUIRE_APPROVAL \
+  -c accountId=${3} \
+  -c region=${4} \
   -c projectName=${5} \
   -c environmentName=${6} #\
   # -c sslCertificateArn=${6}
